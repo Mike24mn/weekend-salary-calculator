c96ebf23c4379f75f3d8e1460571039188387f21
Michael LaFond <Mike24mn@gmail.com>
2024-05-03 15:04:47 -0500

    Created necessary files .html, .js, and .css, also wrote some html code and played with overall design

--- Salary Calculator Assigment/client.js
+++ Salary Calculator Assigment/client.js
@@ -8,6 +8,7 @@ const employees = [
 
 
 
+
 ///* This application should have a form with five inputs that collect a 
 //new employee's *first name, last name, ID number, job title, annual salary*.
 //
