a2fcde4a90b716a849522b6da1a9bb4b7595120c
Michael LaFond <Mike24mn@gmail.com>
2024-05-03 11:44:02 -0500

    I added .js, linked .css, created object employees and gave some traits.

---
+++ Salary Calculator Assigment/client.js
@@ -0,0 +1,13 @@
+const employees = [
+    firstName = ''
+    lastName = ''
+    idNumber = ''
+    jobTitle = '' 
+    annualSalary = ''
+]
+
+
+
+///* This application should have a form with five inputs that collect a 
+//new employee's *first name, last name, ID number, job title, annual salary*.
+//
